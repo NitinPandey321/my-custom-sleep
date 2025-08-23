a1b7747047393de690892e1ed27cd4f6d0688a77
Rajdeep Panwar <rajdeepsinghpanwar972@gmail.com>
2025-08-23 15:36:40 +0530

    Linter

--- config/routes.rb
+++ config/routes.rb
@@ -61,5 +61,5 @@ Rails.application.routes.draw do
     resources :messages, only: [ :create ]
   end
 
-  resource :daily_reflection, only: [:show, :create]
+  resource :daily_reflection, only: [ :show, :create ]
 end
