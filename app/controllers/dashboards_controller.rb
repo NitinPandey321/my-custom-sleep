0f8337e527b2e67eada46276283363ce6bedfe13
Rajdeep Panwar <rajdeepsinghpanwar972@gmail.com>
2025-08-24 10:53:56 +0530

    Realese 1.0

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -15,36 +15,9 @@ class DashboardsController < ApplicationController
   end
 
   def coach
-    if params[:recipient_id].present?
-      @recipient = User.find(params[:recipient_id])
-      @conversation = Conversation.between(current_user.id, @recipient.id).first_or_create
-      if @conversation.id.nil?
-        Conversation.create(sender: @recipient, recipient: current_user)
-        @conversation = Conversation.between(@recipient.id, current_user.id).first_or_create
-      end
-      @messages = @conversation.messages.order(created_at: :asc)
-      current_user.mark_messages_as_read(@recipient)
-      @new_message = @conversation.messages.build
-    end
-
-    if params[:client_id].present?
-      @client = User.find(params[:client_id])
-    end
-    @coach = current_user
-    @clients = @coach.clients
-    # Search
-    if params[:search].present?
-      @clients = @clients.where("first_name ILIKE ? OR last_name ILIKE ?", "%#{params[:search]}%", "%#{params[:search]}%")
-    end
-
-    # Filter by status
-    if params[:status].present? && params[:status] != "All Status"
-      @clients = @clients.where(status: User.statuses[params[:status]])
-    end
-
-    @total_clients = @coach.clients.count
-    @active_clients = @coach.clients.count # You can add more specific logic for active clients
-    @pending_approvals = 5 # Placeholder - you can implement this based on your business logic
+    session[:user_id] = nil
+    flash[:notice] = "You have been logged out"
+    redirect_to login_path
   end
 
   def admin
