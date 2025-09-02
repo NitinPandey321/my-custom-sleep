acdb3418b33040d2601ae9e835ff384893833408
Rajdeep Panwar <rajdeepsinghpanwar972@gmail.com>
2025-09-02 15:58:28 +0530

    remove unwanted

--- app/controllers/omniauth_controller.rb
+++
@@ -1,6 +0,0 @@
-# app/controllers/omniauth_controller.rb
-class OmniauthController < ApplicationController
-  def passthru
-    render status: 404, plain: "Not found. Authentication passthru."
-  end
-end
