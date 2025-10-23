31ce70c22fe81694e9ac439cd9544b20c8450d59
Rajdeep Panwar <rajdeepsinghpanwar972@gmail.com>
2025-10-23 17:20:36 +0530

    pst to default

--- lib/time_zone_normalizer.rb
+++ lib/time_zone_normalizer.rb
@@ -1,26 +1,35 @@
 # lib/time_zone_normalizer.rb
 module TimeZoneNormalizer
   TIMEZONE_MAP = {
-    # Common legacy aliases
+    # IANA legacy / aliases
     "Asia/Calcutta" => "Asia/Kolkata",
     "US/Eastern" => "America/New_York",
     "US/Central" => "America/Chicago",
     "US/Mountain" => "America/Denver",
     "US/Pacific" => "America/Los_Angeles",
+
+    # Windows names
+    "Pacific Standard Time" => "America/Los_Angeles",
+    "Eastern Standard Time" => "America/New_York",
+    "Central Standard Time" => "America/Chicago",
+    "Mountain Standard Time" => "America/Denver",
+    "India Standard Time" => "Asia/Kolkata",
+    "GMT Standard Time" => "Europe/London",
+
+    # Other UTC aliases
     "Etc/UTC" => "UTC",
     "GMT" => "UTC",
     "Coordinated Universal Time" => "UTC"
   }.freeze
 
+  FALLBACK_TIMEZONE = "America/Los_Angeles"
+
   def self.normalize(name)
     return "UTC" if name.blank?
 
-    # Direct match or alias
     mapped = TIMEZONE_MAP[name] || name
-
-    # If invalid, fallback to UTC
-    ActiveSupport::TimeZone[mapped] ? mapped : "UTC"
+    ActiveSupport::TimeZone[mapped] ? mapped : FALLBACK_TIMEZONE
   rescue
-    "UTC"
+    FALLBACK_TIMEZONE
   end
 end
