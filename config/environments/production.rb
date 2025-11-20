325ddcafc401c1baca0c820509cb619203018d3d
Rajdeep Panwar <rajdeepsinghpanwar972@gmail.com>
2025-11-20 09:51:31 +0530

    fix delevery

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,7 @@ Rails.application.configure do
   config.active_record.attributes_for_inspect = [ :id ]
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = false
+  config.action_mailer.perform_deliveries = true
 
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
