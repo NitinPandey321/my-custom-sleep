6f0ecebe0cbcf1afc12f90c2ccba213145db3587
Rajdeep Panwar <rajdeepsinghpanwar972@gmail.com>
2026-01-11 11:54:37 +0530

    fix the image

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,6 +16,7 @@ Rails.application.configure do
 
   # Turn on fragment caching in view templates.
   config.action_controller.perform_caching = true
+  config.action_mailer.asset_host = "https://mycustomsleepjourney.com"
 
   # Cache assets for far-future expiry since they are all digest stamped.
   config.public_file_server.headers = { "cache-control" => "public, max-age=#{1.year.to_i}" }
